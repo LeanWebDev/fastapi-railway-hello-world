d1eb6f53ef91725ebd145f7868dc4ae8079be9f3
Samir Azzouz <s.azzouz97@gmail.com>
2022-06-10 20:34:50 +0100

    test env variable access

--- main.py
+++ main.py
@@ -6,15 +6,23 @@ import random
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
-    return {"Hello": "World from FastAPI"}
+    return {"Hello": "World from FastAPI", "test": os.getenv("TEST_VARIABLE")}
+
 
 # get random number between min(default:0) and max(default:9)
 @app.get("/random/")
 def get_random(min: Optional[int] = 0, max: Optional[int] = 9):
     rval = random.randint(min, max)
-    return { "value": rval }
+    return {"value": rval}
+
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", default=5000)), log_level="info")
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", default=5000)),
+        log_level="info",
+    )
